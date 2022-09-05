72e8fb4120dbdd9113aad35e161e9258f6bb4f49
dn <dn@t1000.su>
2022-09-05 10:13:38 +0300

    feat(prot): add protobuf build

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Quote-api
 
+### Dependencies
+```bash
+sudo apt install -y protobuf-compiler libprotobuf-dev
+```
+
 ```bash
 cp .env.dist .env
 
---
+++ build.rs
@@ -0,0 +1,28 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .out_dir("src")
+        .compile(
+            &[
+                // "contracts/common.proto",
+                "contracts/instruments.proto",
+                "contracts/marketdata.proto",
+                "contracts/operations.proto",
+                "contracts/orders.proto",
+                "contracts/sandbox.proto",
+                "contracts/stoporders.proto",
+                "contracts/users.proto",
+            ],
+            &["contracts/"],
+        )?;
+
+    std::fs::rename(
+        "src/tinkoff.public.invest.api.contract.v1.rs",
+        "src/tinkoff.protobuf.rs"
+    );
+
+    Ok(())
+}
+
+
